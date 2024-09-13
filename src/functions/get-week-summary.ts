fcbccb52a8bdefcfe3f73fa0c10b75029c8bb1b4
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-09-12 21:33:30 -0400

    tipando propriedade goalsPerDay

--- src/functions/get-week-summary.ts
+++ src/functions/get-week-summary.ts
@@ -57,6 +57,15 @@ export async function getWeekSummary() {
       .groupBy(goalsCompletedInWeek.completedAtDate)
   );
 
+  type GoalsPerDay = Record<
+    string,
+    {
+      id: string;
+      title: string;
+      completedAt: string;
+    }[]
+  >;
+
   const result = await db
     .with(goalsCreatedUpToWeek, goalsCompletedInWeek, goalsCompletedByWeekDay)
     .select({
@@ -66,7 +75,7 @@ export async function getWeekSummary() {
       total: sql /*sql*/`
         (SELECT SUM(${goalsCreatedUpToWeek.desiredWeeklyFrequency}) FROM ${goalsCreatedUpToWeek})
       `.mapWith(Number),
-      goalsPerDay: sql /*sql*/`
+      goalsPerDay: sql<GoalsPerDay> /*sql*/`
         JSON_OBJECT_AGG(
           ${goalsCompletedByWeekDay.completedAtDate},
           ${goalsCompletedByWeekDay.completions}
@@ -76,6 +85,6 @@ export async function getWeekSummary() {
     .from(goalsCompletedByWeekDay);
 
   return {
-    summary: result,
+    summary: result[0],
   };
 }
