9a9bbdc49eb646ab961ef85124ea99953b24ca45
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-09-22 17:08:01 -0400

    consertando erro server.ts

--- src/http/server.ts
+++ src/http/server.ts
@@ -9,39 +9,11 @@ import { createCompletionGoalRoute } from "./routes/create-completion-goal";
 import { getPendingGoalsRoute } from "./routes/get-pending-goals";
 import { getWeekSummaryRoute } from "./routes/get-week-summary";
 import fastifyCors from "@fastify/cors";
-import swagger from "@fastify/swagger";
-import swaggerUI from "@fastify/swagger-ui";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
   origin: "*",
-});
-
-app.register(swagger, {
-  swagger: {
-    info: {
-      title: "API Documentation",
-      description: "API documentation for your project",
-      version: "1.0.0",
-    },
-    host: "localhost:3333",
-    schemes: ["http"],
-    consumes: ["application/json"],
-    produces: ["application/json"],
-  },
-});
-
-app.register(swaggerUI, {
-  routePrefix: "/docs", // URL para acessar a documentação
-  swagger: {
-    info: {
-      title: "API Documentation",
-      version: "1.0.0",
-    },
-  },
-  staticCSP: true,
-  transformStaticCSP: (header) => header,
 });
 
 app.setValidatorCompiler(validatorCompiler);
